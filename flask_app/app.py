422297d1382e417cf33cd840c8e450e434721c04
Benny <admin@Admins-MacBook-Air.local>
2023-07-25 14:46:14 -0400

    working on routes

--- flask_app/app.py
+++ flask_app/app.py
@@ -2,7 +2,17 @@ from flask import Flask, render_template, request, redirect, url_for
 import requests
 import database_functions as dbf
 
-# ---------- Login/Signup ----------
+current_email = None
+current_user = None
+
+# ---------- Homepage ----------
+# Home
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+# ---------- Account Information ----------
 # Login
 @app.route("/login")
 def login():
@@ -59,7 +69,42 @@ def logout():
     global current_user
     global current_email
 
-    current_user = ""
-    current_email = ""
+    current_user = None
+    current_email = None
 
     return render_template('home.html', current_user=current_user, logout_message='Successfully Logged Out!')
+
+
+# ---------- Find ----------
+@app.route('/find')
+def find():
+
+
+# ---------- Message ----------
+@app.route('/message')
+def message():
+
+
+# ---------- Profile ----------
+@app.route('/profile')
+def profile():
+    global current_email
+
+    information = dbf.get_user_information(current_email)
+
+    return render_template('profile.html', information=information)
+
+# ---------- About ----------
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# ---------- Premium ----------
+@app.route('/premium')
+def premium():
+    global current_email
+
+    is_premium = dbf.verify_premium(current_email)
+
+    return render_template('premium.html', is_premium=is_premium)
+
--- flask_app/database_functions.py
+++ flask_app/database_functions.py
@@ -2,13 +2,13 @@ import sqlite3
 
 database_link = "./static/data/database.db"
 
-# ---------- Login/Signup ----------
+# ---------- Account Information ----------
 # Verify User
 def verify_user(email, password):
     connect = sqlite3.connect(database_link)
     cursor = connect.cursor()
 
-    result = cursor.execute('SELECT * FROM userbase WHERE email = ? AND password = ?', (email, password,))
+    result = cursor.execute('SELECT * FROM users WHERE email = ? AND password = ?', (email, password,))
         
     information = {
         "username": ""
@@ -27,7 +27,22 @@ def signup_user(username, email, password):
     connect = sqlite3.connect(database_link)
     cursor = connect.cursor()
 
-    cursor.execute('INSERT INTO userbase(username, email, password) VALUES (?, ?, ?)', (username, email, password,))
+    cursor.execute('INSERT INTO users(username, email, password) VALUES (?, ?, ?)', (username, email, password,))
 
     connect.commit()
     connect.close()
+
+# Verify Premium
+def verify_premium(email):
+    connect = sqlite3.connect(database_link)
+    cursor = connect.cursor()
+
+    result = cursor.execute('SELECT is_premium FROM users WHERE email = ?', (email,))
+        
+    is_premium = False
+    
+    if result[0][0] == "True":
+        is_premium = True
+
+    connect.close()
+    return is_premium
